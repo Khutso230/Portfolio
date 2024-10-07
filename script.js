03837da35a521f53673b9adb643ec8759b8c53b8
khutso masia <166388915+Khutso230@users.noreply.github.com>
2024-10-07 11:36:48 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,21 @@
+function toggleMenu() {
+  const menu = document.querySelector(".menu-links");
+  const icon = document.querySelector(".hamburger-icon");
+  menu.classList.toggle("open");
+  icon.classList.toggle("open");
+  
+}
+function scrollToContact() {
+  const contactSection = document.getElementById('contact');
+  contactSection.scrollIntoView({ behavior: 'smooth' });
+}
+function scrollToExperience() {
+  document.getElementById("experience").scrollIntoView({
+    behavior: "smooth"
+  });
+}
+function scrollToProjects() {
+  document.getElementById("projects").scrollIntoView({
+    behavior: "smooth"
+  });
+}
